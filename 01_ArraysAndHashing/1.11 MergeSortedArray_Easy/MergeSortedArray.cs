73482453409da30085b93b17bc85f802c40e26be
Minh Duy Truong <duytm112@noreply.github.com>
2026-01-14 09:31:07 -0600

    Practice

--- 01_ArraysAndHashing/1.11 MergeSortedArray_Easy/MergeSortedArray.cs
+++ 01_ArraysAndHashing/1.11 MergeSortedArray_Easy/MergeSortedArray.cs
@@ -34,10 +34,10 @@ public class MergeSortedArray
     {
         for (int i = m - 1; i < nums1.Length - 1; i++)
         {
-            for (int j = n - 1; j < nums2.Length; j++)
+            for (int j = 0; j < nums2.Length; j++)
             {
                 nums1[i] = nums2[j];
-                continue;
+                break;
             }
         }
         Array.Sort(nums1);
